7569a7a183a1f55da7ef17840e6b7d36f789aa0c
Jonas <jonas.d.oberg@gmail.com>
2025-02-10 12:42:19 +0100

    början på scania

--- ScaniaLastbil.java
+++ ScaniaLastbil.java
@@ -1,2 +1,38 @@
-public class ScaniaLastbil {
+import java.awt.*;
+
+public class ScaniaLastbil extends Vehicle {
+
+    public final int TruckBedAngle = 0;
+
+    public Scania(){
+        super("Scania", 4, 175, "North", Color.blue)
+        stopEngine();
+    }
+
+    protected void RaiseTruckBed(int degrees) {
+        if (getCurrentSpeed() > 0) {
+            System.out.println("Truck must be stationary to raise or lower the truckbed");
+        } else if (degrees < 0 || degrees > 70){
+            System.out.println("Invalid integer. Must be within the limits 0-70");
+        } else if (TruckBedAngle + degrees >= 70) {
+            TruckBedAngle = 70;
+        } else {
+            TruckBedAngle += degrees;
+        }
+    }
+
+    protected void LowerTruckBed(int degrees) {
+        //ska det vara en this. före currentspeed eller ska den skrivas om med en getter?
+        //känns som det inte ska returnas strings här utan borde ge något annat, eller vara ett annat typ av fall.
+        if (getCurrentSpeed() > 0){
+            System.out.println("Truck must be stationary to raise or lower the truckbed");
+        } else if (degrees < 0 || degrees > 70){
+            System.out.println("Invalid integer. Must be within the limits 0-70");
+        } else if (TruckBedAngle + degrees <= 0) {
+            TruckBedAngle = 0;
+        } else {
+            TruckBedAngle -= degrees;
+        }
+    }
+
 }
