788dc8af0144b1b5f6cad37be9cfad8e2f874054
crisse4 <melvin.christensson@gmail.com>
2025-02-11 13:21:30 +0100

    yes

---
+++ CarWorkshop.java
@@ -0,0 +1,20 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class CarWorkshop <T extends Vehicle>{
+    public int capacity;
+    private int nrCars;
+    private List<T> vehicles = new ArrayList<>();
+
+    public CarWorkshop(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public void turnInCar() {
+        if (nrCars < this.capacity) {
+            nrCars +=1;}
+        else {
+            throw new IllegalArgumentException("The workshop is full");
+        }
+    }
+}
